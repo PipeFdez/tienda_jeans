6097736cbdfa07792a50903c1d6c23ea0b9a8105
Felipe Fernandez <fernandez.correa98@gmail.com>
2026-04-28 18:24:43 -0400

    Microservicio Jean

--- service-jean/src/main/java/com/musa_jeans/service_jean/ServiceJeanApplication.java
+++ service-jean/src/main/java/com/musa_jeans/service_jean/ServiceJeanApplication.java
@@ -1,13 +1,13 @@
-package com.felipe.jeans;
+package com.musa_jeans.service_jean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JeansApplication {
+public class ServiceJeanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JeansApplication.class, args);
+		SpringApplication.run(ServiceJeanApplication.class, args);
 	}
 
 }
--- service-jean/src/test/java/com/musa_jeans/service_jean/ServiceJeanApplicationTests.java
+++ service-jean/src/test/java/com/musa_jeans/service_jean/ServiceJeanApplicationTests.java
@@ -1,10 +1,10 @@
-package com.felipe.jeans;
+package com.musa_jeans.service_jean;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JeansApplicationTests {
+class ServiceJeanApplicationTests {
 
 	@Test
 	void contextLoads() {
