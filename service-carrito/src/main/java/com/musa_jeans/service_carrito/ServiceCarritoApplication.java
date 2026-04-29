20197f693f505aa8f1f437e78e8c321ad390be09
Felipe Fernandez <fernandez.correa98@gmail.com>
2026-04-28 21:35:59 -0400

    Base 5 microservicios

---
+++ service-carrito/src/main/java/com/musa_jeans/service_carrito/ServiceCarritoApplication.java
@@ -0,0 +1,13 @@
+package com.musa_jeans.service_carrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCarritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCarritoApplication.class, args);
+	}
+
+}
